9b31ba0243fa4092ee3a6b45829f3f8b8e173685
Raul <developer.rbs@gmail.com>
2024-11-17 01:17:43 +0200

    Implemented playRound function using a switch statement for determining round outcomes.
    Updated the getComputerChoice function to return lowercase values for consistency with the match variable.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice () {
     let result;
 
@@ -6,15 +9,15 @@ function getComputerChoice () {
     switch(result){
 
         case 1 : 
-        return "Rock";
+        return "rock";
         break;
 
         case  2 :
-        return "Paper";
+        return "paper";
         break;
 
         case  3 :
-        return "Scissors";
+        return "scissors";
         break;
 
         default :
@@ -27,8 +30,6 @@ function getComputerChoice () {
 function getHumanChoice () {
     let humanChoice = prompt("Please choose one of the following items from the arsenal: 1) Rock, 2) Paper, 3) Scissors .");
 
-    
-
     if(humanChoice === ''){
         console.log('Please choose an item from the arsenal.')
         return null;
@@ -37,11 +38,54 @@ function getHumanChoice () {
     humanChoice = humanChoice.toLowerCase();
     
     if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
-        return `Your choice: ${humanChoice}`;
+        return humanChoice;
     } else {
         console.log("The item you chose is currently unavailable in this game. Please choose another one.");
         return null;
     }
 }
 
-console.log(getHumanChoice())
+//console.log(getHumanChoice())
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+function playRound (humanChoice, computerChoice) {
+
+    const match = humanChoice+computerChoice;
+
+    switch (match) {
+
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorsscissors':
+            console.log('Draw!');
+            break;
+        
+        case 'rockpaper':
+        case 'paperscissors':
+        case 'scissorsrock':
+            computerScore++;
+            console.log('Computer Win this round !');
+            break;
+        
+        case 'rockscissors':
+        case 'scissorspaper':
+        case 'paperrock':
+            humanScore++;
+            console.log("You win this round !");
+            break;
+
+        default :
+            console.log('Looks like we have to forget this round');
+            break;
+    }
+
+    console.log(`Your choice:${humanChoice}`);
+    console.log(`The Computer's choice:${computerChoice}`);
+    console.log(`Your score is:${humanScore}`);
+    console.log(`The Computer's score:${computerScore}`);
+
+};
+
+playRound(humanChoice, computerChoice);
