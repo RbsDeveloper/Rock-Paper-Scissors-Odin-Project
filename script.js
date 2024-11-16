5f6acda72a755002373043fa5208ee6212a456cc
Raul <developer.rbs@gmail.com>
2024-11-16 23:29:53 +0200

    First function of the game created: the function responsible for the computer's choice.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hello fellas')
+function getComputerChoice () {
+    let result;
+
+    result = Math.floor(Math.random()*3)+1;
+
+    switch(result){
+
+        case 1 : 
+        return "Rock";
+        break;
+
+        case  2 :
+        return "Paper";
+        break;
+
+        case  3 :
+        return "Scissors";
+        break;
+
+        default :
+        return "The Computer is currently unable to choos from the arsenal"
+    }
+}
+
+console.log(getComputerChoice())
